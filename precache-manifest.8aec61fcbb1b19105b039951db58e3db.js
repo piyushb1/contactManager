833efe472dda1c3ad5d0c2f8f41fb339e27984ac
Piyush Bankar <piyushbankar18@gmail.com>
2020-10-12 18:59:46 +0530

    Updates

---
+++ precache-manifest.8aec61fcbb1b19105b039951db58e3db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f1cec94d96c234f256712c2c8a17ea",
+    "url": "/contactManager/index.html"
+  },
+  {
+    "revision": "f8988e1d5c5565462fde",
+    "url": "/contactManager/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "d8b83fd5f77efdd2b84c",
+    "url": "/contactManager/static/css/main.281a20d3.chunk.css"
+  },
+  {
+    "revision": "f8988e1d5c5565462fde",
+    "url": "/contactManager/static/js/2.fd2b57a4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactManager/static/js/2.fd2b57a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8b83fd5f77efdd2b84c",
+    "url": "/contactManager/static/js/main.9a8e5759.chunk.js"
+  },
+  {
+    "revision": "6497b123bdced0ab5dc9",
+    "url": "/contactManager/static/js/runtime-main.40072245.js"
+  }
+]);
